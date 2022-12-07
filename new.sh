cf33ce89c510a9cec46952564f736ba001faf5c6
Junhyuk Lee <ljhjoon00@gmail.com>
2022-12-07 16:47:34 +0900

    feat: Add automation script for upload and form generation

---
+++ new.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "File is not made. Please enter a title"
+    exit 1
+fi
+
+TITLE=$(echo $1 | tr '_' ' ')
+echo $TITLE
+
+if [ -z "$2" ]
+then
+    DATE="0000-00-00"
+else
+    DATE=$2
+fi
+
+CONTENTS="# $TITLE\n\n## $TITLE 란/이란\n\n## References\n\n1. "
+PATH=./pending
+FILENAME=$DATE\;$TITLE.md
+
+echo -e $CONTENTS > "$PATH/$FILENAME"
