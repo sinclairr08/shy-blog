533c5070d32267777d195404bb88e26e5800d817
sinclairr08 <ljhjoon00@gmail.com>
2024-06-30 22:06:24 +0900

    fix: change opeators to run do.sh file on sh

--- scripts/do.sh
+++ scripts/do.sh
@@ -4,13 +4,13 @@ echo "Select what to do"
 echo "(U) Upload a post | (C) Create a post | (S) Create a Subject | (Else) Exit"
 read JOB
 
-if [ "$JOB" == "U" ]
+if [ "$JOB" = "U" ]
 then
     ./scripts/upload.sh
-elif [ "$JOB" == "C" ]
+elif [ "$JOB" = "C" ]
 then
     ./scripts/new.sh
-elif [ "$JOB" == "S" ]
+elif [ "$JOB" = "S" ]
 then
     ./scripts/new_subject.sh
 else
