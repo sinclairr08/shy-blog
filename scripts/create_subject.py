1eae6ab3c92668c26b15167f8cbe4c425e6113ed
sinclairr08 <ljhjoon00@gmail.com>
2024-07-10 01:02:37 +0900

    feat: add depth 2 subjects at _index.md of depth 1

--- scripts/create_subject.py
+++ scripts/create_subject.py
@@ -7,6 +7,7 @@ title: "{title}"
 """
     supplemental = f"""
 # {title}
+
 """
 
     with open(path, 'w', encoding='utf-8') as file:
@@ -28,18 +29,27 @@ def create_subject(subject, path, depth):
         create_index_file(subject, f"{path}/_index.md", depth)
 
 
+def add_sub_subject(subject, sub_subject, sub_subject_ko, index_fn):
+    line = f"- [{sub_subject_ko}](/{subject}/{sub_subject})\n"
+    
+    with open(index_fn, 'a', encoding='utf-8') as file:
+        file.write(line)
+
+
+
 if __name__ == "__main__":
     print("§ Enter a subject name > ", end = "")
     subject = input()
     create_subject(subject, f"content/{subject}", depth = 1)
-
-    print("§ Do you want to make sub-subject? Enter anything > ", end = "")
-    yn = input()
-
-    if yn == "":
-        quit()
     
-    print("§ Enter a sub-subject name > ", end = "")
+    print("§ Enter a sub-subject name (blank: quit) > ", end = "")
     sub_subject = input()
 
+    if sub_subject == "":
+        quit()
+
+    print("§ Enter a sub-subject name in korean > ", end = "")
+    sub_subject_ko = input()
+
     create_subject(sub_subject, f"content/{subject}/{sub_subject}", depth = 2)    
+    add_sub_subject(subject, sub_subject, sub_subject_ko, f"content/{subject}/_index.md")
