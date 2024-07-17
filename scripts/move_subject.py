a6eb8cd75c82f934c95a5d342fed8f289892bae0
sinclairr08 <ljhjoon00@gmail.com>
2024-07-17 20:49:46 +0900

    feat: add default subject to moving subject

--- scripts/move_subject.py
+++ scripts/move_subject.py
@@ -1,12 +1,20 @@
 import shutil
 import os
 
+DEFAULT_1 = "algorithm"
+DEFAULT_2 = "boj"
+
 if __name__ == "__main__":
     dest_1 = input("§ Enter subject depth 1 > ")
     dest_2 = input("§ Enter subject depth 2 > ")
+
+    if dest_1 == "":
+        print(f"Substitute depth 1 to {DEFAULT_1}")
+        dest_1 = DEFAULT_1
     
-    if dest_1 == "" or dest_2 == "":
-        quit("Blank path")
+    if dest_2 == "":
+        print(f"Substitute depth 2 to {DEFAULT_2}")
+        dest_2 = DEFAULT_2
     
     dest_dir = f"content/{dest_1}/{dest_2}"
     if not os.path.isdir(dest_dir):
